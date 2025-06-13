5895e17ff268bae2d27ce6071dcd0ad2d5ab901c
amy-wu <amywu177@gmail.com>
2025-06-14 01:04:57 +1000

    ?? TAILWIND

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -2,11 +2,12 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
+
   const [list, setList] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("/api/route");
+      const res = await fetch("/api");
       const data = await res.json();
       setList(data);
     };
@@ -14,11 +15,14 @@ export default function Home() {
     fetchData();
   }, []);
 
-
-   
   return (
-    <div>
-      <p>{JSON.stringify(list)}</p>
+    // <div className="bg-white width" >
+    //   <p className="bg-white">{JSON.stringify(list)}</p>
+    // </div>
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <div className="p-10 bg-white rounded-lg shadow-lg">
+        <p className="text-xl text-pink-600">Tailwind is now working!</p>
+      </div>
     </div>
   );
 }
