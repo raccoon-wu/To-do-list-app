fb9260f006e375aaff53ebc422e5f8853cfd2c07
amy-wu <amywu177@gmail.com>
2025-06-14 03:56:51 +1000

    Mantine Ui installed

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,10 +1,14 @@
+'use client';
 import "./globals.css";
+import { MantineProvider } from '@mantine/core';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,5 +1,6 @@
 "use client"
 import { useEffect, useState } from "react";
+import { Table } from '@mantine/core';
 
 export default function Home() {
 
@@ -11,18 +12,25 @@ export default function Home() {
       const data = await res.json();
       setList(data);
     };
-
     fetchData();
   }, []);
 
+  // const rows = list.map(() => (
+
+  // ))
   return (
-    // <div className="bg-white width" >
-    //   <p className="bg-white">{JSON.stringify(list)}</p>
-    // </div>
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <div className="p-10 bg-white rounded-lg shadow-lg">
-        <p className="text-xl text-pink-600">Tailwind is now working!</p>
-      </div>
+    <div className="bg-slate-700 w-screen h-screen justify-center items-center" >
+      {/* <pre className="bg-blue">{JSON.stringify(list, null, 2)}</pre> */}
+    <Table className="bg-slate-500 w-1/2 h-1/2">
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Title</Table.Th>
+          <Table.Th>Description</Table.Th>
+          <Table.Th>Status</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      {/* <Table.Tbody>{list}</Table.Tbody> */}
+    </Table>
     </div>
   );
 }
